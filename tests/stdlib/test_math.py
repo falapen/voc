159e4dcb69136906d691af6b7bd8c4e1232670cc
Anton Carlsson <mail@antoncarlsson.se>
2018-09-11 00:36:23 +0200

    Added two more tests for testing the math module.

--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -3,11 +3,24 @@ from unittest import expectedFailure
 from ..utils import TranspileTestCase
 
 class MathModuleTests(TranspileTestCase):
-    #######################################################
-    # sin
+    # Trigonometric functions
     @expectedFailure
     def test_sin(self):
         self.assertCodeExecution("""
             import math
             print(math.sin(1))
+            """)
+
+    @expectedFailure
+    def test_cos(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.cos(1))
+            """)
+
+    @expectedFailure
+    def test_tan(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.tan(1))
             """)
