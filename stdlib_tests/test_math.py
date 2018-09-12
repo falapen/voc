932027b6bab9b3c381ee25a1ba6fe26321774c84
nurre <nurre13@gmail.com>
2018-09-12 22:25:13 +0200

    Running hypot, toDegrees and toRadians using voc.

---
+++ stdlib_tests/test_math.py
@@ -0,0 +1,8 @@
+from java.lang import Math
+
+print(Math.hypot(3,4))
+print(Math.hypot(0,5))
+print(Math.toDegrees(Math.PI))
+print(Math.toDegrees(2*Math.PI))
+print(Math.toRadians(180))
+print(Math.toRadians(270))
