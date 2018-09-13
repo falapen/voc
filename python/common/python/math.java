10315766d254c7274282009b3799144f8c8d8553
falapen <filipgolander@gmail.com>
2018-09-13 10:56:09 +0200

    Re-commit since sqrt-method causes compilation error. sqrt not working

--- python/common/python/math.java
+++ python/common/python/math.java
@@ -36,11 +36,11 @@ public class math extends org.python.types.Module {
             __doc__ = "Create and return a new object.  See help(type) for accurate signature.",
             args = {"value"}
     )
-    public static org.python.types.Object sqrt(org.python.types.Object value) {
+    public static /*org.python.types.Object*/double sqrt(org.python.types.Object value) {
         double val = 0; 
         if(value instanceof org.python.types.Float) {
             val = Math.sqrt(((org.python.types.Float) value).value);
         }
-        return org.python.types.Float(val);
+        return val;
     }
 }
