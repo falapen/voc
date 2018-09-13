7608103f971829427633c3d5eb9016955c7db9ef
falapen <filipgolander@gmail.com>
2018-09-13 10:53:50 +0200

    Added basic tests for ceil, floor and sqrt methods aswell as basic java implementation of them

---
+++ python/common/python/math.java
@@ -0,0 +1,46 @@
+package python;
+
+import java.lang.Math;
+
+@org.python.Module(
+        __doc__ = "Math-things"
+    )
+
+public class math extends org.python.types.Module {
+
+    @org.python.Method(
+            __doc__ = "Create and return a new object.  See help(type) for accurate signature.",
+            args = {"value"}
+    )
+    public static org.python.types.Object ceil(org.python.types.Object value) {
+        int val = 0; 
+        if(value instanceof org.python.types.Float) {
+            val = (int) Math.ceil(((org.python.types.Float) value).value);
+        }
+        return org.python.types.Int.getInt(val);
+    }
+
+    @org.python.Method(
+            __doc__ = "Create and return a new object.  See help(type) for accurate signature.",
+            args = {"value"}
+    )
+    public static org.python.types.Object floor(org.python.types.Object value) {
+        int val = 0; 
+        if(value instanceof org.python.types.Float) {
+            val = (int) Math.floor(((org.python.types.Float) value).value);
+        }
+        return org.python.types.Int.getInt(val);
+    }
+    
+    @org.python.Method(
+            __doc__ = "Create and return a new object.  See help(type) for accurate signature.",
+            args = {"value"}
+    )
+    public static org.python.types.Object sqrt(org.python.types.Object value) {
+        double val = 0; 
+        if(value instanceof org.python.types.Float) {
+            val = Math.sqrt(((org.python.types.Float) value).value);
+        }
+        return org.python.types.Float(val);
+    }
+}
