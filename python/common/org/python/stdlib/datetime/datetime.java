de6d64d52e5bcac8720ddf9289ea4653447a4864
falapen <filipgolander@gmail.com>
2018-09-19 17:07:52 +0200

    removed commented code

--- python/common/org/python/stdlib/datetime/datetime.java
+++ python/common/org/python/stdlib/datetime/datetime.java
@@ -12,14 +12,6 @@ public class datetime extends org.python.types.Module {
         super();
     }
 
-    //@org.python.Method(
-    //    __doc__ = "Returns a Date-object at the specified date",
-    //    args = {"year", "month", "day"}
-    //)
-    //public static org.python.Object date(org.python.Object year, org.python.Object month, org.python.Object day) {
-    //    return new org.python.stdlib.datetime.Date(year, month, day);
-    //}
-
     @org.python.Attribute
     public static org.python.Object date;
 }
