63abfd931cf415a5fb9438af390c6eb5fd4c5cc1
falapen <filipgolander@gmail.com>
2018-09-21 11:18:37 +0200

    Fixed the goddamn __getattributes__ function so that it calls the super-method in case the desired attribute is not one of the attributes with custom access

--- python/common/org/python/stdlib/datetime/Date.java
+++ python/common/org/python/stdlib/datetime/Date.java
@@ -51,7 +51,7 @@ public class Date extends org.python.types.Object {
             args = {"year", "month", "day"}
     )
     public Date(org.python.Object year, org.python.Object month, org.python.Object day) {
-        //super();
+        super();
         if(year instanceof org.python.types.Int) {
             if(((org.python.types.Int) year).value > 0 || ((org.python.types.Int) year).value < 10000) {
                 this.year = (org.python.types.Int) year;
@@ -89,30 +89,15 @@ public class Date extends org.python.types.Object {
     
 
     //Interface methods
-    public org.python.types.Object __getattribute__(java.lang.String name) {
+    public org.python.Object __getattribute__(java.lang.String name) {
         if(name.equals("year")) return this.year;
         if(name.equals("month")) return this.month;
         if(name.equals("day")) return this.day;
-        return null;
+        return super.__getattribute__(name);
     };
     
     public void __setattr__(java.lang.String name, org.python.Object value) {
         throw new org.python.exceptions.AttributeError("attribute \'" + name + "\' of 'datetime.date' objects is not writable");
-        //if(name.equals("year")) {
-        //    if(value instanceof org.python.types.Int) {
-        //        this.year = (org.python.types.Int) value;
-        //    }
-        //};
-        //if(name.equals("month")) {
-        //    if(value instanceof org.python.types.Int) {
-        //        this.month = (org.python.types.Int) value;
-        //    }
-        //};
-        //if(name.equals("day")) {
-        //    if(value instanceof org.python.types.Int) {
-        //        this.day = (org.python.types.Int) value;
-        //    }
-        //};
     };
 
     @org.python.Method(
@@ -140,22 +125,18 @@ public class Date extends org.python.types.Object {
     )
     public static org.python.Object fromtimestamp(org.python.Object timestamp) {
         java.time.LocalDate date;
-
         if((timestamp instanceof org.python.types.Int) || (timestamp instanceof org.python.types.Float)) {
             date = 
                 java.time.Instant.ofEpochSecond(((org.python.types.Int) timestamp.__int__()).value)
                 .atZone(java.time.ZoneId.systemDefault()).toLocalDate();
-
             long yearMax = ((org.python.stdlib.datetime.Date) max).year.value;
             long yearMin = ((org.python.stdlib.datetime.Date) min).year.value;
-
             if (date.getYear() > yearMax || date.getYear() < yearMin) {
                 throw new org.python.exceptions.ValueError("year " + date.getYear() + " is out of range");
             }
         } else {
             throw new org.python.exceptions.TypeError("an integer is required (got type str)");
         }
-
         return new org.python.stdlib.datetime.Date(
             org.python.types.Int.getInt(Long.valueOf(date.getYear())), 
             org.python.types.Int.getInt(Long.valueOf(date.getMonthValue())), 
@@ -173,14 +154,17 @@ public class Date extends org.python.types.Object {
     }
 
     @org.python.Method(
-            __doc__ = "Return the day of the week represented by the date.\nMonday == 1 ... Sunday == 7"
+            __doc__ = "Return the day of the week represented by the date.\nMonday == 1 ... Sunday == 7", 
+            args = {}
     )
     public org.python.Object isoweekday() {
         java.time.LocalDate date = java.time.LocalDate.of((int) this.year.value, (int) this.month.value, (int) this.day.value);
         long weekday = (long) date.getDayOfWeek().getValue(); // date.isoweekday() uses one indexing
-
-        return org.python.types.Int.getInt(weekday);
+        org.python.types.Int wd = org.python.types.Int.getInt(weekday);
+        return wd;
     }
+
+    
 
     @org.python.Method(
             __doc__ = "Return a 3-tuple containing ISO year, week number, and weekday."
