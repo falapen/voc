3b4d9fa1397b96cded2862dcf2bec332469ff7b3
Anton Carlsson <mail@antoncarlsson.se>
2018-09-25 15:16:11 +0200

    Added more tests to improve coverage, mainly for the update method.

--- python/common/org/python/tests/OrderedDictTest.java
+++ python/common/org/python/tests/OrderedDictTest.java
@@ -140,6 +140,114 @@ class OrderedDictTest {
     }
 
     @Test
+    void setItemWithDictAsIterableTest() {
+        org.python.Object[] args = {null};
+        Map<String, org.python.Object> kwargs = new HashMap<String, org.python.Object>();
+        OrderedDict odict = new OrderedDict(args, kwargs);
+
+        kwargs.put("c", Int.getInt(3));
+        Dict dict = new Dict(args, kwargs);
+        odict.update(dict, null);
+        kwargs.remove("c");
+        assertEquals("OrderedDict([('c', 3)])", odict.__str__().toJava());
+
+        kwargs.put("b", Int.getInt(2));
+        dict = new Dict(args, kwargs);
+        odict.update(dict, null);
+        kwargs.remove("b");
+        assertEquals("OrderedDict([('c', 3), ('b', 2)])", odict.__str__().toJava());
+
+        kwargs.put("a", Int.getInt(1));
+        dict = new Dict(args, kwargs);
+        odict.update(dict, null);
+        kwargs.remove("a");
+        assertEquals("OrderedDict([('c', 3), ('b', 2), ('a', 1)])", odict.__str__().toJava());
+    }
+
+    @Test
+    void setItemWithTupleAsIterableTest() {
+        org.python.Object[] args = {null};
+        Map<String, org.python.Object> kwargs = new HashMap<String, org.python.Object>();
+        OrderedDict odict = new OrderedDict(args, kwargs);
+
+        List tuple_list = new List();
+        ArrayList<org.python.Object> tuple = new ArrayList<>(2);
+        tuple.add(new Str("c"));
+        tuple.add(org.python.types.Int.getInt(3));
+        tuple_list.append(new org.python.types.Tuple(tuple));
+        odict.update(tuple_list, null);
+        assertEquals("OrderedDict([('c', 3)])", odict.__str__().toJava());
+
+        tuple = new ArrayList<>(2);
+        tuple.add(new Str("b"));
+        tuple.add(org.python.types.Int.getInt(2));
+        tuple_list.append(new org.python.types.Tuple(tuple));
+        odict.update(tuple_list, null);
+        assertEquals("OrderedDict([('c', 3), ('b', 2)])", odict.__str__().toJava());
+
+        tuple = new ArrayList<>(2);
+        tuple.add(new Str("a"));
+        tuple.add(org.python.types.Int.getInt(1));
+        tuple_list.append(new org.python.types.Tuple(tuple));
+        odict.update(tuple_list, null);
+        assertEquals("OrderedDict([('c', 3), ('b', 2), ('a', 1)])", odict.__str__().toJava());
+    }
+
+    @Test
+    void setItemWithListAsIterableTest() {
+        org.python.Object[] args = {null};
+        Map<String, org.python.Object> kwargs = new HashMap<String, org.python.Object>();
+        OrderedDict odict = new OrderedDict(args, kwargs);
+
+        List list_of_lists = new List();
+        ArrayList<org.python.Object> list = new ArrayList<>(2);
+        list.add(new Str("c"));
+        list.add(org.python.types.Int.getInt(3));
+        list_of_lists.append(new org.python.types.List(list));
+        odict.update(list_of_lists, null);
+        assertEquals("OrderedDict([('c', 3)])", odict.__str__().toJava());
+
+        list = new ArrayList<>(2);
+        list.add(new Str("b"));
+        list.add(org.python.types.Int.getInt(2));
+        list_of_lists.append(new org.python.types.List(list));
+        odict.update(list_of_lists, null);
+        assertEquals("OrderedDict([('c', 3), ('b', 2)])", odict.__str__().toJava());
+
+        list = new ArrayList<>(2);
+        list.add(new Str("a"));
+        list.add(org.python.types.Int.getInt(1));
+        list_of_lists.append(new org.python.types.List(list));
+        odict.update(list_of_lists, null);
+        assertEquals("OrderedDict([('c', 3), ('b', 2), ('a', 1)])", odict.__str__().toJava());
+    }
+
+    @Test
+    void setItemWrongSizedPairs() {
+        org.python.Object[] args = {null};
+        Map<String, org.python.Object> kwargs = new HashMap<String, org.python.Object>();
+        OrderedDict odict = new OrderedDict(args, kwargs);
+
+        List list_of_lists = new List();
+        ArrayList<org.python.Object> list = new ArrayList<>(3);
+        list.add(new Str("c"));
+        list.add(org.python.types.Int.getInt(3));
+        list.add(org.python.types.Int.getInt(123));
+        list_of_lists.append(new org.python.types.List(list));
+        assertThrows(org.python.exceptions.ValueError.class, () -> {
+            odict.update(list_of_lists, null);
+        });
+
+        List tuple_list = new List();
+        ArrayList<org.python.Object> tuple = new ArrayList<>(1);
+        tuple.add(new Str("c"));
+        tuple_list.append(new org.python.types.Tuple(tuple));
+        assertThrows(org.python.exceptions.ValueError.class, () -> {
+            odict.update(tuple_list, null);
+        });
+    }
+
+    @Test
     void iterTest() {
         org.python.Object[] args = {null};
         Map<String, org.python.Object> kwargs = new HashMap<String, org.python.Object>();
@@ -148,8 +256,8 @@ class OrderedDictTest {
         kwargs.put("a", Int.getInt(1));
         OrderedDict od = new OrderedDict(args, kwargs);
         java.util.List<String> chars = new ArrayList<String>(Arrays.asList("a", "b", "c"));
-        Iterator<String> charsIter = chars.iterator();
 
+        Iterator<String> charsIter = chars.iterator();
         org.python.Object iter = od.__iter__();
         try {
             while (true) {
