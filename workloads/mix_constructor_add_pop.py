ad2be84f1f2258765ef7f58415c60227aeeba922
Mauro José Pappaterra <mauropappaterra@gmail.com>
2018-10-04 16:35:49 +0200

    Update mix_constructor_add_pop.py

--- workloads/mix_constructor_add_pop.py
+++ workloads/mix_constructor_add_pop.py
@@ -1,26 +1,46 @@
-"""
-Creates 22000 OrderedDicts. Add 100 elements, removes 50 elements does 50 move to end operations on each dict.
-"""
-
+#
+#Benchmarking routing for OrderedDict
+#
 from collections import OrderedDict
-import random
 import time
-start = time.clock()
 
-dictArray = []
+X = 100000 # number of Dictionaries to create
+Y = 10000 # number of elements to populate the dictionaries with
 
-for i in range (22000):
-    dict = OrderedDict()
-    dictArray.append(dict)
+dictionary_array = []
 
-for d in range(len(dictArray)):
-    dict = dictArray[d]
+# Create X dictionaries
+start = time.clock() # start clock
 
-    for i in range(100):
-        dict[i] = i
-    for i in range(50):
-        dict.pop(i)  
-    for a in range(51, 100):
-        dict.move_to_end(a)
+for i in range (X):
+    new_dictionary = OrderedDict()
+    dictionary_array.append(new_dictionary)
 
-print(time.clock()-start)
+print("Created " + str(X) + " dictionaries in -> " + str((time.clock() - start))) # stop clock, print time
+
+# Populate all X dictionaries with Y elements each
+start = time.clock() # start clock
+
+for dictionary in dictionary_array:
+    for index, element in enumerate(dictionary):
+        dictionary[index] = index
+
+print("Populated all " + str(X) + " dictionaries with " + str(Y) + " elements each in -> "  + str((time.clock() - start))) # stop clock, print time
+
+# Updated all Y elements on each dictionary
+start = time.clock() # start clock
+
+for dictionary in dictionary_array:
+    for element in dictionary:
+        element = element * 2
+
+print("Updated all " + str(Y) + " elements in " + str(X) + " dictionaries in -> "  + str((time.clock() - start))) # stop clock, print time
+
+# Pop all Y elements on each dictionary
+start = time.clock() # start clock
+
+for dictionary in dictionary_array:
+    for index, element in enumerate(dictionary):
+        dictionary.popitem()
+
+print("Popped all " + str(Y) + " elements in " + str(X) + " dictionaries in -> "  + str((time.clock() - start))) # stop clock, print time
