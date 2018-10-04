42a2fefc4df5ec4e0f8d4155b6ef8c4340ec3cc6
Fredrik Sandelin <fredriksandelin94@gmail.com>
2018-10-04 17:08:31 +0200

    Pushed todays work

--- python/common/org/python/workloads/java/UpdateCopyItemsPopClearWorkload.java
+++ python/common/org/python/workloads/java/UpdateCopyItemsPopClearWorkload.java
@@ -4,6 +4,20 @@ import org.python.stdlib.collections.OrderedDict;
 
 public class UpdateCopyItemsPopClearWorkload {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        org.python.stdlib.collections.OrderedDict orderedDict = new org.python.stdlib.collections.OrderedDict(new org.python.Object[]{null} , new java.util.HashMap<java.lang.String, org.python.Object>());
+
+        org.python.types.Str[] names = new org.python.types.Str[]{new org.python.types.Str("Harry"), new org.python.types.Str("Ross"),
+            new org.python.types.Str("Bruce"), new org.python.types.Str("Cook"), new org.python.types.Str("Carolyn"),
+            new org.python.types.Str("Morgan"), new org.python.types.Str("Albert"), new org.python.types.Str("Walker"),
+            new org.python.types.Str("Randy"), new org.python.types.Str("Reed"), new org.python.types.Str("Larry"),
+            new org.python.types.Str("Barnes"), new org.python.types.Str("Lois"), new org.python.types.Str("Wilson"),
+            new org.python.types.Str("Jesse"), new org.python.types.Str("Campbell"), new org.python.types.Str("Ernest"),
+            new org.python.types.Str("Rogers"), new org.python.types.Str("Theresa"), new org.python.types.Str("Patterson"),
+            new org.python.types.Str("Henry"), new org.python.types.Str("Simmons"), new org.python.types.Str("Michelle"),
+            new org.python.types.Str("Perry"), new org.python.types.Str("Frank"), new org.python.types.Str("Butler")};
+
+
+
+
     }
 }
