ad236ec30f4fd0e4f43ebff493bd27cde9635a41
Fredrik Sandelin <fredriksandelin94@gmail.com>
2018-10-04 14:12:59 +0200

    Added workload insert_large

---
+++ workloads/insert_random.py
@@ -0,0 +1,11 @@
+"""
+Inserts 650000 random numbers between 0 and 99 in an OrderedDict
+"""
+from collections import OrderedDict
+import time
+import random
+start = time.clock()
+orderedDict = OrderedDict()
+for i in range(650000):
+    orderedDict[i] = random.randint(0,100)
+print(time.clock()-start)
