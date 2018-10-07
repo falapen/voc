53115e8cb2089e7b9f0236796670459d82f7f24d
JohanWindahl <johan.windahl@gmail.com>
2018-10-07 14:40:07 +0200

    Init mix_copy_popitem_first_and_last.java

---
+++ python/common/org/python/workloads/mix_copy_popitem_first_and_last.java
@@ -0,0 +1,12 @@
+package python.common.org.python.workloads;
+
+import org.python.stdlib.collections.OrderedDict;
+import org.python.types.Int;
+import java.util.*;
+
+public class mix_copy_popitem_first_and_last {
+
+    public static void main(String args[]) {
+
+    }
+}
