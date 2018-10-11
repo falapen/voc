d450067b1c87af03da12cf06aa9dad9e1dd11b37
Anton Carlsson <mail@antoncarlsson.se>
2018-10-11 13:30:43 +0200

    Updated the test to count the time and increased the workload.

--- python/common/org/python/workloads/KeysAndFromKeysTest.java
+++ python/common/org/python/workloads/KeysAndFromKeysTest.java
@@ -14,22 +14,25 @@ public class KeysAndFromKeysTest {
 
         org.python.Object[] args = {null};
         java.util.Map kwargs = new java.util.HashMap<java.lang.String, org.python.Object>();
+        long start = System.nanoTime();
         OrderedDict orderedDict = new OrderedDict(args, kwargs);
         OrderedDict updateDict;
 
-        for (int i=0; i<2500; i++) {
+        for (int i=0; i<10000; i++) {
             kwargs.put(String.valueOf(i), Int.getInt(i));
             updateDict = new OrderedDict(args, kwargs);
             orderedDict.update(null, updateDict);
             kwargs.remove(String.valueOf(i));
         }
 
-        for (int i=0; i<2500; i++) {
+        for (int i=0; i<10000; i++) {
             odKeys.add(orderedDict.keys());
         }
 
-        for (int i=0; i<2500; i++) {
+        for (int i=0; i<10000; i++) {
             odList.add(OrderedDict.fromkeys(odKeys.get(i), null));
         }
+        long end = System.nanoTime();
+        System.out.println((end-start)/Math.pow(10, 9));
     }
 }
