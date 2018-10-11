2a40b3cf2ab2baa57b8b5192b1c2d6dc5483749d
Mauro José Pappaterra <mauropappaterra@gmail.com>
2018-10-11 19:03:22 +0200

    Bugs fixes

--- workloads/mix_constructor_add_pop.py
+++ workloads/mix_constructor_add_pop.py
@@ -1,46 +1,50 @@
 #
-#Benchmarking routing for OrderedDict
+#Benchmarking routing for OrderedDict by Mauro José Pappaterra
 #
+
 from collections import OrderedDict
 import time
 
-X = 100000 # number of Dictionaries to create
-Y = 10000 # number of elements to populate the dictionaries with
+X = 10000 # number of Dictionaries to create
+Y = 1000 # number of elements to populate the dictionaries with
 
-dictionary_array = []
+dict_array = []
 
-# Create X dictionaries
+# Create  X number of dictionaries
+start = time.clock() # start clock
+start_total =  start
+
+for i in range (X):
+    dict = OrderedDict()
+    dict_array.append(dict)
+
+print("Created " + str(X) + " dictionaries in -> " + str(round((time.clock() - start),2))) # stop clock, print time
+
+# Populate all X dictionaries with Y elements
 start = time.clock() # start clock
 
 for i in range (X):
-    new_dictionary = OrderedDict()
-    dictionary_array.append(new_dictionary)
+    for e in range (Y):
+        dict_array[i][e] = 'Hello'
 
-print("Created " + str(X) + " dictionaries in -> " + str((time.clock() - start))) # stop clock, print time
+print("Populated all " + str(X) + " dictionaries with " + str(Y) + " elements each in -> "  + str(round((time.clock() - start),2))) # stop clock, print time
 
-# Populate all X dictionaries with Y elements each
+# Update Y values in all X dictionaries
 start = time.clock() # start clock
 
-for dictionary in dictionary_array:
-    for index, element in enumerate(dictionary):
-        dictionary[index] = index
+for i in range (X):
+    for e in range (Y):
+        dict_array[i][e] += ' World!'
 
-print("Populated all " + str(X) + " dictionaries with " + str(Y) + " elements each in -> "  + str((time.clock() - start))) # stop clock, print time
+print("Updated all " + str(Y) + " elements in " + str(X) + " dictionaries in -> "  + str(round((time.clock() - start),2))) # stop clock, print time
 
-# Updated all Y elements on each dictionary
+# Pop all Y elements in all X dictionaries
 start = time.clock() # start clock
 
-for dictionary in dictionary_array:
-    for element in dictionary:
-        element = element * 2
+for i in range (X):
+    for e in range (Y):
+        dict_array[i].popitem()
 
-print("Updated all " + str(Y) + " elements in " + str(X) + " dictionaries in -> "  + str((time.clock() - start))) # stop clock, print time
+print("Popped all " + str(Y) + " elements in " + str(X) + " dictionaries in -> "  + str(round((time.clock() - start),2))) # stop clock, print time
 
-# Pop all Y elements on each dictionary
-start = time.clock() # start clock
-
-for dictionary in dictionary_array:
-    for index, element in enumerate(dictionary):
-        dictionary.popitem()
-
-print("Popped all " + str(Y) + " elements in " + str(X) + " dictionaries in -> "  + str((time.clock() - start))) # stop clock, print time
+print("Total Time - > "  + str(round((time.clock() - start_total),2))) # stop clock, print time )
